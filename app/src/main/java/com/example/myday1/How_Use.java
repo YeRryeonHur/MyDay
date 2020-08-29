c34e4bb0f49257f1131980b1ed73a448b0578450
Lee-Sieon <leese5phone@gmail.com>
2020-08-29 23:35:20 +0900

    Myday 글자 수정

--- app/src/main/java/com/example/myday1/How_Use.java
+++ app/src/main/java/com/example/myday1/How_Use.java
@@ -1,5 +1,6 @@
 package com.example.myday1;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,5 +10,14 @@ public class How_Use extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.how_use);
+    }
+
+    public void onBackPressed(){
+        super.onBackPressed();
+        Intent intent = new Intent(getApplicationContext(), colorchange.class);
+        startActivity(intent);
+        overridePendingTransition(0,0);
+        finish();
+
     }
 }
