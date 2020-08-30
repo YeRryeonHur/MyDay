e929fbd489deba13fea5cbf11e55578fecdfd6ec
Lee-Sieon <leese5phone@gmail.com>
2020-08-30 22:47:00 +0900

    기능설명 변경

---
+++ app/src/main/java/com/example/myday1/Fragment1.java
@@ -0,0 +1,25 @@
+package com.example.myday1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment1 extends Fragment {
+    public Fragment1(){}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+       LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.frag1, container, false);
+        return layout;
+    }
+}
